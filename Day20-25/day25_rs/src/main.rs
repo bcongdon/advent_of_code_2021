73d2ca253b477bcb4d3a00fbe89a9ee4e493a890
Benjamin <ben@congdon.dev>
2021-12-25 08:04:26 -0800

    Day25 clippy refactor

--- Day20-25/day25_rs/src/main.rs
+++ Day20-25/day25_rs/src/main.rs
@@ -17,10 +17,10 @@ impl Default for Tile {
 
 impl Display for Tile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let c = match self {
-            &Self::Empty => '.',
-            &Self::EastCucumber => '>',
-            &Self::SouthCucumber => 'v',
+        let c = match *self {
+            Self::Empty => '.',
+            Self::EastCucumber => '>',
+            Self::SouthCucumber => 'v',
         };
         write!(f, "{}", c)
     }
@@ -51,46 +51,29 @@ fn iterate_grid(grid: &Array2<Tile>) -> (Array2<Tile>, bool) {
     let mut out = grid.clone();
     let mut moved = false;
 
-    let new_east_locs = out
-        .indexed_iter()
-        .filter(|(_, tile)| **tile == Tile::EastCucumber)
-        .map(|((x, y), _)| {
-            let nx = (x + 1) % out.shape()[0];
-            if out[(nx, y)] == Tile::Empty {
-                ((x, y), (nx, y))
-            } else {
-                ((x, y), (x, y))
+    let movement_directions = vec![(Tile::EastCucumber, (1, 0)), (Tile::SouthCucumber, (0, 1))];
+    for (tile, (dx, dy)) in movement_directions {
+        let moves = out
+            .indexed_iter()
+            .filter(|(_, t)| **t == tile)
+            .map(|((x, y), _)| {
+                let nx = (x + dx) % out.shape()[0];
+                let ny = (y + dy) % out.shape()[1];
+                if out[(nx, ny)] == Tile::Empty {
+                    ((x, y), (nx, ny))
+                } else {
+                    ((x, y), (x, y))
+                }
+            })
+            .collect::<Vec<_>>();
+        for (p1 @ (x, y), p2 @ (nx, ny)) in moves {
+            if p1 == p2 {
+                continue;
             }
-        })
-        .collect::<Vec<_>>();
-    for (p1 @ (x, y), p2 @ (nx, ny)) in new_east_locs {
-        if p1 == p2 {
-            continue;
+            moved = true;
+            out[(x, y)] = Tile::Empty;
+            out[(nx, ny)] = tile;
         }
-        moved = true;
-        out[(x, y)] = Tile::Empty;
-        out[(nx, ny)] = Tile::EastCucumber;
-    }
-
-    let new_south_locs = out
-        .indexed_iter()
-        .filter(|(_, tile)| **tile == Tile::SouthCucumber)
-        .map(|((x, y), _)| {
-            let ny = (y + 1) % out.shape()[1];
-            if out[(x, ny)] == Tile::Empty {
-                ((x, y), (x, ny))
-            } else {
-                ((x, y), (x, y))
-            }
-        })
-        .collect::<Vec<_>>();
-    for (p1 @ (x, y), p2 @ (nx, ny)) in new_south_locs {
-        if p1 == p2 {
-            continue;
-        }
-        moved = true;
-        out[(x, y)] = Tile::Empty;
-        out[(nx, ny)] = Tile::SouthCucumber;
     }
 
     (out, moved)
